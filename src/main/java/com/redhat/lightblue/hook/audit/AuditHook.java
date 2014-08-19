c026d6f03138519787c855be50f1eb093be9a697
Naveen Malik <nmalik@redhat.com>
2014-08-19 14:59:06 -0400

    Hopefully fixed the deploy after success config for travis

--- src/main/java/com/redhat/lightblue/hook/audit/AuditHook.java
+++ src/main/java/com/redhat/lightblue/hook/audit/AuditHook.java
@@ -27,7 +27,7 @@ public class AuditHook implements CRUDHook {
 
     public static final String ERR_MISSING_ID = "audit-hook:MissingID";
 
-    protected static final class AuditData {
+    protected final class AuditData {
         Path path;
         String pre;
         String post;
